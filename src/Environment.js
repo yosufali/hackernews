1e49f4a858fabe23dda831578f5d93b38215abfb
Yosuf Ali <yosufali@users.noreply.github.com>
2017-09-14 21:31:04 +0200

    Added relay endpoint and graphql schema.

--- src/Environment.js
+++ src/Environment.js
@@ -12,7 +12,7 @@ const store = new Store(new RecordSource())
 // 3
 const network = Network.create((operation, variables) => {
   // 4
-  return fetch('__RELAY_API_ENDPOINT__', {
+  return fetch('https://api.graph.cool/relay/v1/cj7kssuuk03mp0178cksg46h5', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
