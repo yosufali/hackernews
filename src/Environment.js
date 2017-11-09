b8e09b0fc8e9834a5889d8cc82987814e7e0f203
Yosuf Ali <yosufali@users.noreply.github.com>
2017-11-09 17:52:17 +0000

    Added authorization header to fetch to make sure request is authenticated at graphcool server.

--- src/Environment.js
+++ src/Environment.js
@@ -1,10 +1,12 @@
 // 1
+import { GC_AUTH_TOKEN } from './constants'
 const {
   Environment,
   Network,
   RecordSource,
   Store,
 } = require('relay-runtime')
+
 
 // 2
 const store = new Store(new RecordSource())
@@ -16,7 +18,8 @@ const network = Network.create((operation, variables) => {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem(GC_AUTH_TOKEN)}`
     },
     body: JSON.stringify({
       query: operation.text,
